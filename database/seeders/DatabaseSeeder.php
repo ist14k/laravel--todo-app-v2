b18b0a1a5b80d354cffac5a10d3cb5246ceaa5e6
ist14k <istiak_akash@outlook.com>
2025-09-22 22:14:59 +0600

    Refactor DatabaseSeeder to include task and note seeding for Test User

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,10 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ])->each(function ($user) {
+            $user->tasks()->saveMany(\App\Models\Task::factory(rand(3, 15))->create(['user_id' => $user->id])->each(function ($task) {
+                $task->notes()->saveMany(\App\Models\Note::factory(rand(3, 15))->create(['task_id' => $task->id]));
+            }));
+        });
     }
 }
